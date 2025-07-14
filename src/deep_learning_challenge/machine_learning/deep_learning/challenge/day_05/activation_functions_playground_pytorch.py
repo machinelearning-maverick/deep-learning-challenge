0d71d6d27c993119c1759bc573af6864767a464c
Machine Learning Maverick <github@machinelearningmaverick.com>
2025-07-14 20:01:20 +0100

    feat(day_05): refactor activation functions plotting and for PyTorch activations - separate data from plotting

--- src/deep_learning_challenge/machine_learning/deep_learning/challenge/day_05/activation_functions_playground_pytorch.py
+++ src/deep_learning_challenge/machine_learning/deep_learning/challenge/day_05/activation_functions_playground_pytorch.py
@@ -3,32 +3,37 @@ import torch.nn.functional as F
 
 import matplotlib.pyplot as plt
 
-# Input values
-x = torch.linspace(-5, 5, 200)
 
-# Activation functions
-activations = {
-    "ReLU": F.relu(x),
-    "Sigmoid": torch.sigmoid(x),
-    "Tanh": torch.tanh(x),
-    "LeakyReLU": F.leaky_relu(x, negative_slope=0.1),
-    "Softplus": F.softplus(x),
-    "Softmax": F.softmax(x),
-}
+def get_activations_pytorch(x_vector = torch.linspace(-3, 3, 200)):
+    # Activation functions
+    activations = {
+        "ReLU": F.relu(x_vector),
+        "Sigmoid": torch.sigmoid(x_vector),
+        "Tanh": torch.tanh(x_vector),
+        "LeakyReLU": F.leaky_relu(x_vector, negative_slope=0.1),
+        "Softplus": F.softplus(x_vector),
+        "Softmax": F.softmax(x_vector),
+    }
 
-# Plot
-plt.figure(figsize=(10, 6))
-for name, y in activations.items():
-    plt.plot(x.numpy(), y.numpy(), label=name)
+    return x_vector.numpy(), {k: v.numpy() for k, v in activations.items()}
 
-plt.plot(x.numpy(), x.numpy(), label="x (raw input)", linestyle="--", color="gray")
 
-plt.title("Activation Functions (PyTorch)")
-plt.xlabel("x")
-plt.ylabel("activation(x)")
-plt.legend()
-plt.grid(True)
+def plot_activations_pytorch():
+    x, activations = get_activations_pytorch()
 
-# plt.savefig("activation_functions_playground_pytorch.png")
+    # Plot
+    plt.figure(figsize=(10, 6))
+    for name, y in activations.items():
+        plt.plot(x, y, label=name)
 
-plt.show()
+    plt.plot(x, x, label="x (raw input)", linestyle="--", color="gray")
+
+    plt.title("Activation Functions (PyTorch)")
+    plt.xlabel("x")
+    plt.ylabel("activation(x)")
+    plt.legend()
+    plt.grid(True)
+
+    # plt.savefig("activation_functions_playground_pytorch.png")
+
+    plt.show()
