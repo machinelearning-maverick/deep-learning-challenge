e4cde24facd4363dea07cd152d2ad7b40b3ee8ae
Machine Learning Maverick <github@machinelearningmaverick.com>
2025-07-15 21:24:10 +0100

    feat(refactor, test): move Keras code into a function to be able to test it

--- src/deep_learning_challenge/machine_learning/deep_learning/challenge/day_06/from_tensor_to_model_sdf_keras.py
+++ src/deep_learning_challenge/machine_learning/deep_learning/challenge/day_06/from_tensor_to_model_sdf_keras.py
@@ -2,22 +2,25 @@ import numpy as np
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, ReLU
-from tensorflow.keras.utils import plot_model
 
-# Dummy input: batch of samples 4, 3 features each
-x = np.random.randn(4, 3)
 
-# Define a simple Multi-Layer Perceptron
-model = Sequential([
-    Dense(5, input_shape=(3,), name="dense_1"), # input: 3 features; output: 5 neurons
-    ReLU(name="relu_1"),
-    Dense(2, name="dense_2"), # hidden; input: 5 neurons; output: 2 classes/units
-])
+def keras_shape_debug(batch_size=4, input_dim=3):
+    # Dummy input: batch of samples 4, 3 features each
+    x = np.random.randn(batch_size, input_dim)
 
-# Forward pass
-output = model.predict(x)
-print(f"Input shape: {x.shape}")
-print(f"Output shape: {output.shape}")
+    # Define a simple Multi-Layer Perceptron
+    model = Sequential([
+        Dense(5, input_shape=(input_dim,), name="dense_1"), # input: 3 features; output: 5 neurons
+        ReLU(name="relu_1"),
+        Dense(2, name="dense_2"), # hidden; input: 5 neurons; output: 2 classes/units
+    ])
 
-# Inspect shapes; model summary
-model.summary()
+    # Forward pass
+    output = model.predict(x, verbose=0)
+    print(f"Input shape: {x.shape}")
+    print(f"Output shape: {output.shape}")
+
+    # Inspect shapes; model summary
+    model.summary()
+
+    return [x.shape, (batch_size, 5), output.shape]
