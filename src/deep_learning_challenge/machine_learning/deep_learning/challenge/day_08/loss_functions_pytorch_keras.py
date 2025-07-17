908691d9663bb96c55e77285c16b6b5e7e05f7be
Machine Learning Maverick <github@machinelearningmaverick.com>
2025-07-17 20:52:53 +0100

    feat(pytorch, logits): add BCE with logits anlong with test

--- src/deep_learning_challenge/machine_learning/deep_learning/challenge/day_08/loss_functions_pytorch_keras.py
+++ src/deep_learning_challenge/machine_learning/deep_learning/challenge/day_08/loss_functions_pytorch_keras.py
@@ -3,9 +3,16 @@ import torch.nn as nn
 
 
 def bce_loss_pytorch(y_pred, y_true):
-
     loss_fn = nn.BCELoss()
     loss = loss_fn(y_pred, y_true)
 
     print(f"PyTorch BCE Loss: {loss.item()}")
+    return loss
+
+
+def bce_logits_loss_pytorch(logits, y_true):
+    loss_fn = nn.BCEWithLogitsLoss()
+    loss = loss_fn(logits, y_true)
+
+    print(f"PyTorch BCEWithLogits Loss: {loss.item()}")
     return loss
--- tests/machine_learning/deep_learning/challenge/day_08/test_loss_functions_pytorch_keras.py
+++ tests/machine_learning/deep_learning/challenge/day_08/test_loss_functions_pytorch_keras.py
@@ -2,6 +2,7 @@ import torch
 
 from deep_learning_challenge.machine_learning.deep_learning.challenge.day_08.loss_functions_pytorch_keras import (
     bce_loss_pytorch,
+    bce_logits_loss_pytorch,
 )
 
 
@@ -12,3 +13,13 @@ def test_bce_loss_pytorch():
 
     loss = bce_loss_pytorch(y_pred, y_true)
     assert loss < 0.2
+
+
+def test_bce_logits_loss_pytorch():
+    # Simulated predictions (probabilities) and targets
+    y_true = torch.tensor([[1.0], [0.0]], dtype=torch.float32)
+    # raw outputs before applying activation function
+    logits = torch.tensor([[2.0], [-2.0]], dtype=torch.float32)
+
+    loss = bce_logits_loss_pytorch(logits, y_true)
+    assert loss < 0.15
