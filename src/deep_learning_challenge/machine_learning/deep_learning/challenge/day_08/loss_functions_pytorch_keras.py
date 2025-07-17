cee2e726f4c79b30a76d18acdb33ca73341f23db
Machine Learning Maverick <github@machinelearningmaverick.com>
2025-07-17 21:01:51 +0100

    feat(pytorch, ce): add CrossEntropy with logits along with test

--- src/deep_learning_challenge/machine_learning/deep_learning/challenge/day_08/loss_functions_pytorch_keras.py
+++ src/deep_learning_challenge/machine_learning/deep_learning/challenge/day_08/loss_functions_pytorch_keras.py
@@ -16,3 +16,13 @@ def bce_logits_loss_pytorch(logits, y_true):
 
     print(f"PyTorch BCEWithLogits Loss: {loss.item()}")
     return loss
+
+
+def ce_logits_loss_pytorch(y_logits, y_true):
+    """Cross-Entropy loss for Multi-Class Classification"""
+
+    loss_fn = nn.CrossEntropyLoss()
+    loss = loss_fn(y_logits, y_true)
+
+    print(f"PyTorch CrossEntropy Loss: {loss}")
+    return loss
--- tests/machine_learning/deep_learning/challenge/day_08/test_loss_functions_pytorch_keras.py
+++ tests/machine_learning/deep_learning/challenge/day_08/test_loss_functions_pytorch_keras.py
@@ -3,6 +3,7 @@ import torch
 from deep_learning_challenge.machine_learning.deep_learning.challenge.day_08.loss_functions_pytorch_keras import (
     bce_loss_pytorch,
     bce_logits_loss_pytorch,
+    ce_logits_loss_pytorch,
 )
 
 
@@ -23,3 +24,13 @@ def test_bce_logits_loss_pytorch():
 
     loss = bce_logits_loss_pytorch(logits, y_true)
     assert loss < 0.15
+
+
+def test_ce_logits_loss_pytorch():
+    # logits shape: [batch, classes]
+    y_logits = torch.tensor([[2.0, 0.5, 0.3], [0.2, 0.1, 2.1]])
+    # y_true - true class indicies
+    y_true = torch.tensor([0, 2])
+
+    loss = ce_logits_loss_pytorch(y_logits, y_true)
+    assert loss < 0.30
